ce8c2f67bbbb5c117a02a3ca1dfe9104612ad06d
jaypalchandel0 <37531848-jaypalchandel0@users.noreply.replit.com>
2024-11-25 06:00:53 +0000

    Agent query: Is the application loading properly now? Can you search for a stock symbol and access the backtesting tab?

--- main.py
+++ main.py
@@ -163,7 +163,6 @@ if ticker:
                                 'Portfolio Value Before': trades_df['portfolio_value'].shift(1).fillna(initial_investment).map('${:,.2f}'.format),
                                 'Opening Price': trades_df['open_price'].map('${:,.2f}'.format),
                                 'Entry Price': trades_df['open_price'].map('${:,.2f}'.format),
-                                'Predicted Target': trades_df.apply(lambda x: '${:,.2f}'.format(x['predicted_high'] if x['predicted_direction'] == 'UP' else x['predicted_low']), axis=1),
                                 'Exit Price': trades_df.apply(lambda x: '${:,.2f}'.format(x['high_price'] if x['predicted_direction'] == 'UP' else x['low_price']), axis=1),
                                 'Profit($)': trades_df['trade_profit'].map('${:,.2f}'.format),
                                 'Profit(%)': trades_df['profit_percentage'].map('{:,.2f}%'.format),
